f8cab517476ba667adfe7ce3d5df7d49b92717e8
Simona Ruoss <simonaruoss@Simonas-MacBook-Air.local>
2024-08-28 08:42:22 +0200

    Teilaufgabe c) fast fertig

--- Loops.java
+++ Loops.java
@@ -3,8 +3,29 @@ public class Loops {
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
 
     public static void primeFactorisation(int n){
-        
+        if (n <= 1) {
+            System.out.println("Number must be greater than 1");
+        }
+        System.out.printf("Prime factorisation of %d =" , n);
+        int factor = 2;
+        boolean firstfactor = true;
+
+        while (n > 1){
+            while (n % factor == 0){
+                if (!firstfactor){
+                    System.out.print(" * ");
+                }
+                System.out.printf("%d", factor);
+                n /= factor;
+                firstfactor = false;
+
+            }
+            factor += 1;
+        }
+
+        }
     }
+    
     
     public static void main(String[] args){
         
